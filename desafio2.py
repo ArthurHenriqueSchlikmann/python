a1f75cff33cd18fb255cc35ca22323539afb6b65
Arthur <104400862+ArthurHenriqueSchlikmann@users.noreply.github.com>
2024-05-07 16:47:36 -0300

    v3

---
+++ desafio2.py
@@ -0,0 +1,4 @@
+dia = input("dia = ")
+mes = input("mês = ")
+ano = input("ano = ")
+print("Você nasceu dia", dia, "de", mes, "de", ano)
---
+++ desafio3.py
@@ -0,0 +1,3 @@
+n1 = input("Digite o primeiro número: ")
+n2 = input("Digite o segundo número: ")
+print("A soma é:", n1+n2)
